615d2004c59d5cb03f88e4542e39de72e569ae9c
billj290 <billj290@gmail.com>
2021-06-16 22:06:00 +0800

    good version

--- guess_num.py
+++ guess_num.py
@@ -1,12 +1,16 @@
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+	count += 1 # count = count + 1快寫法
 	num = input('你猜數字多少? ')
 	num = int(num)
 	if num == r:
 		print('你猜對了! ')
+		print('這是你猜的第', count, '次')
 		break
 	elif num < r:
 		print('你猜錯了! 答案比較大!')
 	elif num > r:
 		print('你猜錯啦! 答案比較小')
+	print('這是你猜的第', count, '次') #不寫在 if 架構裡面,是因為寫在裡面的話要寫三次,重複了
